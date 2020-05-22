ef4faec0de24b071a120ecc57538151e9a59cb53
apprenticefarter <63342380+apprenticefarter@users.noreply.github.com>
2020-05-22 12:56:57 +0300

    Contact test added

---
+++ addressbook-web-tests/src/test/java/ru/software_test/addressbook/ContactCreateTest.java
@@ -0,0 +1,92 @@
+package ru.software_test.addressbook;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class ContactCreateTest {
+    private WebDriver wd;
+
+
+    @BeforeMethod(alwaysRun = true)
+    public void setUp() throws Exception {
+        wd = new FirefoxDriver();
+        wd.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        login("admin", "secret");
+    }
+
+    private void login(String username, String password) {
+        wd.get("http://localhost:8080/addressbook/");
+        wd.findElement(By.name("user")).click();
+        wd.findElement(By.name("user")).clear();
+        wd.findElement(By.name("user")).sendKeys(username);
+        wd.findElement(By.name("pass")).clear();
+        wd.findElement(By.name("pass")).sendKeys(password);
+        wd.findElement(By.id("LoginForm")).submit();
+    }
+
+    @Test
+    public void testContactCreate() throws Exception {
+        initContactCreate();
+        fillContactForm(new ContactData("Joe", "Ivanovich", "Trump", "Missleaders"));
+        submitContactCreate();
+        returnHomePage();
+    }
+
+    private void returnHomePage() {
+        wd.findElement(By.linkText("home page")).click();
+    }
+
+    private void submitContactCreate() {
+        wd.findElement(By.name("submit")).click();
+    }
+
+    private void fillContactForm(ContactData contact) {
+        wd.findElement(By.name("firstname")).click();
+        wd.findElement(By.name("firstname")).clear();
+        wd.findElement(By.name("firstname")).sendKeys(contact.getFisrtname());
+        wd.findElement(By.name("middlename")).clear();
+        wd.findElement(By.name("middlename")).sendKeys(contact.getMiddlename());
+        wd.findElement(By.name("lastname")).clear();
+        wd.findElement(By.name("lastname")).sendKeys(contact.getLastname());
+        wd.findElement(By.name("company")).click();
+        wd.findElement(By.name("company")).clear();
+        wd.findElement(By.name("company")).sendKeys(contact.getCompany());
+    }
+
+    private void initContactCreate() {
+        wd.findElement(By.linkText("add new")).click();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() throws Exception {
+        wd.quit();
+
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            wd.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            wd.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+}
---
+++ addressbook-web-tests/src/test/java/ru/software_test/addressbook/ContactData.java
@@ -0,0 +1,31 @@
+package ru.software_test.addressbook;
+
+public class ContactData {
+    private final String fisrtname;
+    private final String middlename;
+    private final String lastname;
+    private final String company;
+
+    public ContactData(String fisrtname, String middlename, String lastname, String company) {
+        this.fisrtname = fisrtname;
+        this.middlename = middlename;
+        this.lastname = lastname;
+        this.company = company;
+    }
+
+    public String getFisrtname() {
+        return fisrtname;
+    }
+
+    public String getMiddlename() {
+        return middlename;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+}
