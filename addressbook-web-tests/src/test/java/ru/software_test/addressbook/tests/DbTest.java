27538eca18ac781e00d9475e14fc4946a71c06f7
apprenticefarter <63342380+apprenticefarter@users.noreply.github.com>
2020-06-26 14:49:44 +0300

    SQL tests

--- addressbook-web-tests/src/test/java/ru/software_test/addressbook/tests/DbTest.java
+++ addressbook-web-tests/src/test/java/ru/software_test/addressbook/tests/DbTest.java
@@ -10,6 +10,7 @@ public class DbTest {
     @Test
     public void testDbConnection() {
         Connection conn = null;
+        Groups groups = new Groups();
 
         try {
             conn =
@@ -18,7 +19,6 @@ public class DbTest {
 
             Statement st = conn.createStatement();
             ResultSet rs = st.executeQuery("select group_id,group_name,group_header,group_footer from group_list");
-            Groups groups = new Groups();
             while (rs.next()) {
                 groups.add(new GroupData().withId(rs.getInt("group_id")).withName(rs.getString("group_name"))
                         .withHeader(rs.getString("group_header")).withFooter(rs.getString("group_footer")));
@@ -30,6 +30,8 @@ public class DbTest {
             System.out.println(groups);
         } catch (SQLException ex) {
             // handle any errors
+
+
             System.out.println("SQLException: " + ex.getMessage());
             System.out.println("SQLState: " + ex.getSQLState());
             System.out.println("VendorError: " + ex.getErrorCode());
